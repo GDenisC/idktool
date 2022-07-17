bfdcab9493922e193c496bde14bd3195063edeac
GDenisC <92527725+GDenisC@users.noreply.github.com>
2022-07-17 19:42:57 +0700

    bugfix

--- idktools/color.py
+++ idktools/color.py
@@ -10,7 +10,7 @@ class ColorError(Exception):
         super().__init__(f"Color `{colorname}` not in 0-255 (float) range")
 
 class ColorRGBA:
-    def __init__(self, red: float = 255, green: float = 255, blue: float = 255, alpha: float = 255, *, array: Sequence[float] = ...) -> None:
+    def __init__(self, red: float = 255, green: float = 255, blue: float = 255, alpha: float = 255, *, array: Sequence[float] = None) -> None:
         if array:
             self.red = array[0]
             self.green = array[1]
@@ -20,13 +20,13 @@ class ColorRGBA:
             except IndexError:
                 self.alpha = 255
             return
-        if red < 0 | red > 255:
+        if (red < 0) & (red > 255):
             raise ColorError('red')
-        if green < 0 | green > 255:
+        if (green < 0) & (green > 255):
             raise ColorError('green')
-        if blue < 0 | blue > 255:
+        if (blue < 0) & (blue > 255):
             raise ColorError('blue')
-        if alpha < 0 | alpha > 255:
+        if (alpha < 0) & (alpha > 255):
             raise ColorError('alpha')
 
         self.red = red
